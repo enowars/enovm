3051e35033622c5453224f715d4ff39452a47524
Benedikt Radtke <benediktradtke@gmail.com>
2019-10-14 22:17:29 +0200

    wörk

--- Vagrant.sh
+++ Vagrant.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 gameserver="$(virsh domifaddr enovm_gameserver | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}')"
 
 echo "Found gameserver at $gameserver"
 
-iptables -A PREROUTING -t nat -p tcp --dport 30000 -j DNAT --to-destination $gameserver:30000
 iptables -I POSTROUTING -t nat -j MASQUERADE
 
 for i in {1..2}
 do
     vulnbox="$(virsh domifaddr enovm_vulnbox$i | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}')"
     port=$(( 65000 + $i ))
+    vpnport=$(( 30000 + $i ))
     echo "Exposing wireguard port of vulnbox$i on $port"
     iptables -A PREROUTING -t nat -p udp --dport $port -j DNAT --to-destination $vulnbox:1194
+    echo "Exposing openvpn server for vulnbox$i on $port"
+    iptables -A PREROUTING -t nat -p tcp --dport $vpnport -j DNAT --to-destination $gameserver:$vpnport
 done
