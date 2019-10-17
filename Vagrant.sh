0ed76206409841490e2c109b085dabf29494f2d6
Benedikt Radtke <benediktradtke@gmail.com>
2019-10-17 13:41:37 +0200

    wörk

--- Vagrant.sh
+++ Vagrant.sh
@@ -13,10 +13,12 @@ do
     vulnbox="$(virsh domifaddr enovm_vulnbox$i | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}')"
     port=$(( 65000 + $i ))
     vpnport=$(( 30000 + $i ))
-    echo "Exposing wireguard port of vulnbox$i on $port"
+    echo "Exposing wireguard port of vulnbox$i on $port udp"
     iptables -A PREROUTING -t nat -p udp --dport $port -j DNAT --to-destination $vulnbox:1194
     echo "Exposing openvpn server for vulnbox$i on $vpnport"
     iptables -A PREROUTING -t nat -p tcp --dport $vpnport -j DNAT --to-destination $gameserver:$vpnport
+    echo "Exposing sshd for vulnbox$i on $port tcp"
+    iptables -A PREROUTING -i enp10s0 -t nat -p tcp --dport $port -j DNAT --to-destination $vulnbox:22
 done
 
 conntrack --flush
