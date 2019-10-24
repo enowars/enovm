6e1eb4fbcb53431b65c8ce0b8d1636a7faefb346
Benedikt Radtke <benediktradtke@gmail.com>
2019-10-25 01:22:13 +0200

    bash is bad

--- Vagrant.sh
+++ Vagrant.sh
@@ -19,13 +19,13 @@ do
     if [[ ($i -ge $local_min_team_id) && ($i -le $local_max_team_id) ]] ; then
         vulnbox="$(virsh domifaddr enovm_vulnbox$i | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}')"
         port=$(( 65000 + $i ))
-        vpnport=$(( 30000 + $i ))
         echo "Exposing wireguard port of vulnbox$i on $port udp"
         iptables -A PREROUTING -t nat -p udp --dport $port -j DNAT --to-destination $vulnbox:1194
         echo "Exposing sshd for vulnbox$i on $port tcp"
         iptables -A PREROUTING -i enp10s0 -t nat -p tcp --dport $port -j DNAT --to-destination $vulnbox:22
     fi
     if [[ $local_min_team_id == "1" ]] ; then
+        vpnport=$(( 30000 + $i ))
         echo "Exposing openvpn server for vulnbox$i on $vpnport"
         iptables -A PREROUTING -t nat -p tcp --dport $vpnport -j DNAT --to-destination $gameserver:$vpnport
     fi
